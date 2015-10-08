786e12d49eab9f7d0a078ae92df1d4bf04fd629e
Steve Gehrman <sgehrman@gmail.com>
2015-10-08 11:57:56 -0700

    added balloon

---
+++ Assets/Party Pack/BalloonClick.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BalloonClick : MonoBehaviour
+{
+
+  // Use this for initialization
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  public void OnClick()
+  {
+
+
+
+    // destroy everything
+    foreach (Transform child in this.transform)
+    {
+      Destroy(child.gameObject);
+    }
+    Destroy(this);
+  }
+}
