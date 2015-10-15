f2f34a27685a3bb9d8ae473577d581d7f63c104b
Steve Gehrman <sgehrman@gmail.com>
2015-10-15 00:07:00 -0700

    cardboard load

---
+++ Assets/CardboardLoader.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardboardLoader : MonoBehaviour
+{
+  public GameObject cardboardPrefab;
+  public bool buildingForCardboard = false;
+
+  // Use this for initialization
+  void Start()
+  {
+    if (buildingForCardboard)
+    {
+      Instantiate(cardboardPrefab, new Vector3(0, 1.2f, 2f), Quaternion.identity);
+
+      Cardboard cardboard = GetComponent<Cardboard>();
+      cardboard.VRModeEnabled = true;
+    }
+  }
+}
