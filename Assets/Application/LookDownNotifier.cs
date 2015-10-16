451eb901999f441eda249851e7f3d0b05ca9435c
Steve Gehrman <sgehrman@gmail.com>
2015-10-16 02:15:44 -0700

    duh

--- Assets/Application/LookDownNotifier.cs
+++ Assets/Application/LookDownNotifier.cs
@@ -4,33 +4,80 @@ using System.Collections;
 public class LookDownNotifier : MonoBehaviour
 {
   public GameObject downArrow;
+  public GameObject sign;
+
+  Vector3 startPosition;
+  Material signMaterial;
+  Material downArrayMaterial;
 
   // Use this for initialization
   void Start()
   {
-    if (downArrow != null)
-      iTween.MoveBy(downArrow, iTween.Hash("y", .05f, "time", .4f, "easeType", iTween.EaseType.easeInOutSine, "loopType", iTween.LoopType.pingPong));
+    if (downArrow == null || sign == null)
+    {
+      Debug.Log("LookDownNotifier: bad parameters");
+      return;
+    }
 
+    startPosition = transform.position;
+    signMaterial = sign.GetComponent<MeshRenderer>().material;
+    downArrayMaterial = downArrow.GetComponent<MeshRenderer>().material;
 
+    signMaterial.color = new Color(1, 1, 1, 0);
+    downArrayMaterial.color = new Color(0, 0, 0, 0);
 
-    iTween.MoveBy(gameObject, iTween.Hash("y", 2f, "time", .5f, "easeType", iTween.EaseType.easeOutExpo, "onupdate", "AnimationUpdateCallback", "oncomplete", "AnimationDoneCallback"));
-
-
+    StartCoroutine(StartAnimation(1));
   }
 
-  public void AnimationUpdateCallback()
+
+
+  IEnumerator StartAnimation(float delay)
   {
-    Debug.Log("update");
+    yield return new WaitForSeconds(delay);
+
+    // bounces down arrow forever
+    iTween.MoveBy(downArrow, iTween.Hash("y", .03f, "time", .4f, "easeType", iTween.EaseType.easeInOutSine, "loopType", iTween.LoopType.pingPong));
+
+    // move into position and fade in
+    iTween.ValueTo(gameObject, iTween.Hash("from", 0f, "to", 1f, "easetype", iTween.EaseType.easeOutExpo, "onupdate", "AnimationUpdateCallback", "time", 1f, "oncomplete", "AnimationDoneCallback"));
+  }
+
+  public void AnimationUpdateCallback(float progress)
+  {
+    Vector3 vect = gameObject.transform.position;
+
+    vect.y = startPosition.y + (progress * 1);
+
+    gameObject.transform.position = vect;
+
+
+    signMaterial.color = new Color(1, 1, 1, progress);
+    downArrayMaterial.color = new Color(0, 0, 0, progress);
   }
 
   public void AnimationDoneCallback()
   {
-    Debug.Log("done");
+    StartCoroutine(FadeOutAndDestroy(3));
   }
 
-  // Update is called once per frame
-  void Update()
+
+  IEnumerator FadeOutAndDestroy(float delay)
   {
+    yield return new WaitForSeconds(delay);
 
+    iTween.ValueTo(gameObject, iTween.Hash("from", 1f, "to", 0f, "easetype", iTween.EaseType.easeOutExpo, "onupdate", "FadeOutUpdateCallback", "time", .5f, "oncomplete", "FadeOutDoneCallback"));
   }
+
+
+  public void FadeOutUpdateCallback(float progress)
+  {
+    signMaterial.color = new Color(1, 1, 1, progress);
+    downArrayMaterial.color = new Color(0, 0, 0, progress);
+  }
+
+  public void FadeOutDoneCallback()
+  {
+    Destroy(gameObject);
+  }
+
 }
