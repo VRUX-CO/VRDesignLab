677840175c423fd0b5f345f35aa1092ef56437aa
Steve Gehrman <sgehrman@gmail.com>
2015-10-17 16:41:17 -0700

    sdf

--- Assets/LevelMenu/LevelMenuItem.cs
+++ Assets/LevelMenu/LevelMenuItem.cs
@@ -32,12 +32,17 @@ public class LevelMenuItem : MonoBehaviour
 
   public void OnHoverStart()
   {
+    float time = .5f;
+    float scaleBy = 1.05f;
+
     material.color = new Color(0f, 0f, 0f, .2f);
+    iTween.ScaleTo(gameObject, scaleBy * Vector3.one, time);
   }
 
   public void OnHoverEnd()
   {
     material.color = new Color(0f, 0f, 0f, 0f);
+    transform.localScale = new Vector3(1f, 1f, 1f);
   }
 
   public void OnClick()
