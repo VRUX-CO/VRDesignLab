d446f075f4481e50f6a2a04e1a6108b20268c49b
Steve Gehrman <sgehrman@gmail.com>
2015-10-17 20:47:44 -0700

    asdf

---
+++ Assets/Keep User Grounded/CubeSpawner.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeSpawner : MonoBehaviour
+{
+  public GameObject cube;
+  bool running = true;
+  float xOffset = 3;
+
+  void Start()
+  {
+    StartCoroutine(SpawnCube(0));
+  }
+
+  IEnumerator SpawnCube(int count)
+  {
+    for (int x = 0; x < 1; x++)
+    {
+      float startZ = 70;
+      float xRandom = Random.RandomRange(3f, 22f);
+      float zRandom = Random.RandomRange(-10f, 10f);
+
+      Vector3 position = new Vector3(xOffset + xRandom, 0f, startZ + zRandom);
+      if ((count % 2) == 0)
+      {
+        position = new Vector3(-xOffset - xRandom, 0f, startZ + zRandom);
+      }
+
+      Instantiate(cube, position, Quaternion.identity);
+    }
+
+    if (running)
+    {
+      yield return new WaitForSeconds(.2f);
+
+      StartCoroutine(SpawnCube(count + 1));
+    }
+
+  }
+}
---
+++ Assets/Keep User Grounded/CubeSpawnerCube.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeSpawnerCube : MonoBehaviour
+{
+  // Use this for initialization
+  void Start()
+  {
+    float startScale = 3f;
+    float yScale = 40f;
+    float moveTime = 12f;
+
+    Vector3 startPosition = transform.position;
+
+    transform.localScale = new Vector3(startScale, 0, startScale);
+
+    iTween.ScaleTo(gameObject, new Vector3(startScale, yScale, startScale), Random.RandomRange(9f, 13f));
+
+    startPosition.z = -4;
+    iTween.MoveTo(gameObject, iTween.Hash("position", startPosition, "time", moveTime, "easeType", iTween.EaseType.linear, "oncomplete", "OnCompleteCallback"));
+
+
+
+
+
+  }
+
+  public void OnCompleteCallback()
+  {
+    Destroy(gameObject);
+  }
+
+}
