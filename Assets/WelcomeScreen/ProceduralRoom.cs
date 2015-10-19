d0d2c257636e357cbe6245469d4fb1fe68ba9903
Steve Gehrman <sgehrman@gmail.com>
2015-10-19 00:24:16 -0700

    room

---
+++ Assets/WelcomeScreen/ProceduralRoom.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ProceduralRoom : MonoBehaviour
+{
+  public GameObject wallPrefab;
+  List<Material> materials = new List<Material>();
+
+  // Use this for initialization
+  void Start()
+  {
+    MakeSide(0);
+    MakeSide(1);
+    MakeSide(2);
+    MakeSide(3);
+    MakeSide(4);
+    MakeSide(5);
+  }
+
+  public void SetColor(Color color)
+  {
+    foreach (Material mat in materials)
+    {
+      mat.color = color;
+    }
+  }
+
+  // Update is called once per frame
+  void MakeSide(int side)
+  {
+    GameObject wall = Instantiate(wallPrefab) as GameObject;
+    wall.transform.parent = transform;
+
+    materials.Add(wall.GetComponent<MeshRenderer>().material);
+
+    float d = wall.transform.localScale.x;
+
+    switch (side)
+    {
+      case 0:
+        wall.transform.localPosition = new Vector3(0, 0, d);
+        break;
+      case 1:
+        wall.transform.localPosition = new Vector3(-d / 2, 0, d / 2);
+        wall.transform.localRotation = Quaternion.Euler(0, -90, 0);
+        break;
+      case 2:
+        wall.transform.localPosition = new Vector3(d / 2, 0, d / 2);
+        wall.transform.localRotation = Quaternion.Euler(0, 90, 0);
+
+        break;
+      case 3:
+        wall.transform.localPosition = new Vector3(0, 0, 0);
+        wall.transform.localRotation = Quaternion.Euler(0, 180, 0);
+
+        break;
+      case 4:
+        wall.transform.localPosition = new Vector3(0, -d / 2, d / 2);
+        wall.transform.localRotation = Quaternion.Euler(90, 0, 0);
+        break;
+      case 5:
+        wall.transform.localPosition = new Vector3(0, d / 2, d / 2);
+        wall.transform.localRotation = Quaternion.Euler(-90, 0, 0);
+        break;
+    }
+  }
+}
--- Assets/WelcomeScreen/WelcomeHelper.cs
+++ Assets/WelcomeScreen/WelcomeHelper.cs
@@ -3,27 +3,23 @@ using System.Collections;
 
 public class WelcomeHelper : MonoBehaviour
 {
-  public GameObject whiteRoomPrefab;
+  public GameObject proceduralRoomPrefab;
   public GameObject welcomeSignPrefab;
 
-  GameObject whiteRoom, welcomeSign;
+  GameObject proceduralRoom, welcomeSign;
 
-  Material signMaterial, whiteRoomMaterial;
+  Material signMaterial;
 
   // Use this for initialization
   void Start()
   {
-    Vector3 roomPosition = new Vector3(0, 1.2f, 0);
+    Vector3 roomPosition = new Vector3(0, 1.2f, -8);
     Vector3 signPosition = new Vector3(0, 1.2f, 1);
 
-    whiteRoom = Instantiate(whiteRoomPrefab, roomPosition, Quaternion.identity) as GameObject;
+    proceduralRoom = Instantiate(proceduralRoomPrefab, roomPosition, Quaternion.identity) as GameObject;
     welcomeSign = Instantiate(welcomeSignPrefab, signPosition, Quaternion.identity) as GameObject;
 
-    whiteRoom.transform.localScale = new Vector3(5, 5, 5);
-    Utilities.ReverseNormals(whiteRoom);
-
     signMaterial = welcomeSign.GetComponent<MeshRenderer>().material;
-    whiteRoomMaterial = whiteRoom.GetComponent<MeshRenderer>().material;
   }
 
   // Update is called once per frame
@@ -50,24 +46,24 @@ public class WelcomeHelper : MonoBehaviour
   public void FadeOutDoneCallback()
   {
     Destroy(welcomeSign);
-    StartCoroutine(FadeOutAndDestroy(2));
+    StartCoroutine(FadeOutAndDestroy2(0));
   }
 
   IEnumerator FadeOutAndDestroy2(float delay)
   {
     yield return new WaitForSeconds(delay);
 
-    iTween.ValueTo(gameObject, iTween.Hash("from", 1f, "to", 0f, "easetype", iTween.EaseType.easeOutExpo, "onupdate", "FadeOutUpdateCallback2", "time", .5f, "oncomplete", "FadeOutDoneCallback2"));
+    iTween.ValueTo(gameObject, iTween.Hash("from", 1f, "to", 0f, "easetype", iTween.EaseType.easeOutExpo, "onupdate", "FadeOutUpdateCallback2", "time", 3.5f, "oncomplete", "FadeOutDoneCallback2"));
   }
 
   public void FadeOutUpdateCallback2(float progress)
   {
-    whiteRoomMaterial.color = new Color(1, 1, 1, progress);
+    proceduralRoom.GetComponent<ProceduralRoom>().SetColor(new Color(1, 1, 1, progress));
   }
 
   public void FadeOutDoneCallback2()
   {
-    Destroy(whiteRoom);
+    Destroy(proceduralRoom);
   }
 
 }
