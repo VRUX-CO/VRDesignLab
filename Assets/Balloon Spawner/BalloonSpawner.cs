01054e168debaafecbeee10877a4043ad1298a16
L3Mantis <luis@mantisvr.com>
2015-10-20 16:22:58 -0700

    balloon spawn in

--- Assets/Balloon Spawner/BalloonSpawner.cs
+++ Assets/Balloon Spawner/BalloonSpawner.cs
@@ -6,7 +6,7 @@ public class BalloonSpawner : MonoBehaviour
   public GameObject balloonPrefab;                // The enemy prefab to be spawned.
     public Lab1_Flow flow;
     private int balloonpop = 0;
-    private bool spawned;
+    private bool swapped;
     private bool endlab;
 
     // Use this for initialization
@@ -25,23 +25,38 @@ public class BalloonSpawner : MonoBehaviour
   {
         
         yield return new WaitForSeconds(1.0f);
+        if (!swapped)
+        {
+            Vector3 spawnPosition = new Vector3(
+              transform.position.x + Random.Range(1f, 1.5f),
+              transform.position.y,
+              transform.position.z + Random.Range(3f, 4f)
+            );
+            GameObject balloon = Instantiate(balloonPrefab, spawnPosition, Quaternion.identity) as GameObject;
+            BalloonClick click = balloon.GetComponent<BalloonClick>();
+            click.spawner = this;
+            yield return null;
+        }
+        else
+        {
+            Vector3 spawnPosition = new Vector3(
+                 transform.position.x + Random.Range(-2f, -1f),
+                 transform.position.y,
+                 transform.position.z + Random.Range(3f, 4f)
+               );
+            GameObject balloon = Instantiate(balloonPrefab, spawnPosition, Quaternion.identity) as GameObject;
+            BalloonClick click = balloon.GetComponent<BalloonClick>();
+            click.spawner = this;
+            yield return null;
+        }
+            
+       
 
-    Vector3 spawnPosition = new Vector3(
-      transform.position.x + Random.Range(1f, 1.5f),
-      transform.position.y,
-      transform.position.z + Random.Range(3f, 4f)
-    );
-
-    GameObject balloon = Instantiate(balloonPrefab, spawnPosition, Quaternion.identity) as GameObject;
-
-    BalloonClick click = balloon.GetComponent<BalloonClick>();
-    click.spawner = this;
+    /*BalloonClick click = balloon.GetComponent<BalloonClick>();
+    click.spawner = this;*/
         //balloonpop += 1;
         //Debug.Log(balloonpop.ToString());
-        if (balloonpop == 2)
-        {
-            flow.PoppedBalloon();
-        }
+        
         //spawned = false;
         yield return null;
   }
@@ -51,6 +66,11 @@ public class BalloonSpawner : MonoBehaviour
         //spawned = true;
         balloonpop += 1;
         Debug.Log(balloonpop.ToString());
+        if (balloonpop == 2)
+        {
+            flow.PoppedBalloon();
+            swapped = true;
+        }
         SpawnBalloon();
   }
 
