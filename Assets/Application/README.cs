d447bdf7cc1c7a6211bf4788549f4e85e654a3d4
Steve Gehrman <sgehrman@gmail.com>
2015-10-19 22:47:46 -0700

    sfd

--- Assets/Application/README.cs
+++ Assets/Application/README.cs
@@ -4,8 +4,7 @@ using System.Collections;
 /*
 
  * How to build for Cardboard.
- * Click Cardboard Bootstrap and click the checkbox at the bottom called: "Building for Cardboard"
- * Disable the OVRDebugHeadController
+ * Click App Bootstrap and click the checkbox at the bottom called: "Build for Cardboard"
  * Build Settings / Player Settings, turn off Virtual Reality Supported (this activates GearVR launcher)
  
  * How to build for GearVR
