c3be510786451b44bee8d94438d1aa06d7017bc5
Steve Gehrman <sgehrman@gmail.com>
2015-10-19 18:54:19 -0700

    Scene Manager

---
+++ Assets/Application/README.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+
+ * How to build for Cardboard.
+ * Click Cardboard Bootstrap and click the checkbox at the bottom called: "Building for Cardboard"
+ * Disable the OVRDebugHeadController
+ * Build Settings / Player Settings, turn off Virtual Reality Supported (this activates GearVR launcher)
+ 
+ * How to build for GearVR
+ * 
+ * 
+ * How to build for DK2
+ * 
+ * 
+
+*/
---
+++ Assets/Application/Scene Manager/SceneManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour
+{
+  public GameObject cameraFadeScreenPrefab;
+
+  // Use this for initialization
+  void Start()
+  {
+    // AttachFadeScreenToCamera();
+  }
+
+  void AttachFadeScreenToCamera()
+  {
+    GameObject fadeScreen = Instantiate(cameraFadeScreenPrefab) as GameObject;
+
+    fadeScreen.transform.parent = Camera.main.transform;
+
+    fadeScreen.transform.localPosition = new Vector3(0, 0, .4f);
+  }
+}
