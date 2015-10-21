508d4e45c54ec5e1fe125aa7473a3ff2a6ddf5fa
Steve Gehrman <sgehrman@gmail.com>
2015-10-20 22:31:37 -0700

    adsf

--- Assets/Balloon Spawner/BalloonSpawner.cs
+++ Assets/Balloon Spawner/BalloonSpawner.cs
@@ -3,14 +3,14 @@ using System.Collections;
 
 public class BalloonSpawner : MonoBehaviour
 {
-  public GameObject balloonPrefab;                // The enemy prefab to be spawned.
-    public Lab1_Flow flow;
-    private int balloonpop = 0;
-    private bool swapped;
-    private bool endlab;
+  public GameObject balloonPrefab;
+  public Lab1_Flow flow;
+  private int balloonpop = 0;
+  private bool swapped;
+  private bool endlab;
 
-    // Use this for initialization
-    void Start()
+  // Use this for initialization
+  void Start()
   {
     SpawnBalloon();
   }
@@ -23,73 +23,50 @@ public class BalloonSpawner : MonoBehaviour
   // Update is called once per frame
   IEnumerator Spawn()
   {
-        
-        yield return new WaitForSeconds(1.0f);
-        if (!swapped)
-        {
-            Vector3 spawnPosition = new Vector3(
-              transform.position.x + Random.Range(1f, 1.5f),
-              transform.position.y,
-              transform.position.z + Random.Range(3f, 4f)
-            );
-            GameObject balloon = Instantiate(balloonPrefab, spawnPosition, Quaternion.identity) as GameObject;
-            BalloonClick click = balloon.GetComponent<BalloonClick>();
-            click.spawner = this;
-            yield return null;
-        }
-        else
-        {
-            Vector3 spawnPosition = new Vector3(
-                 transform.position.x + Random.Range(-2f, -1f),
-                 transform.position.y,
-                 transform.position.z + Random.Range(3f, 4f)
-               );
-            GameObject balloon = Instantiate(balloonPrefab, spawnPosition, Quaternion.identity) as GameObject;
-            BalloonClick click = balloon.GetComponent<BalloonClick>();
-            click.spawner = this;
-            yield return null;
-        }
-            
-       
+    Vector3 spawnPosition;
 
-    /*BalloonClick click = balloon.GetComponent<BalloonClick>();
-    click.spawner = this;*/
-        //balloonpop += 1;
-        //Debug.Log(balloonpop.ToString());
-        
-        //spawned = false;
-        yield return null;
+    yield return new WaitForSeconds(1.0f);
+
+    if (!swapped)
+    {
+      spawnPosition = new Vector3(
+      transform.position.x + Random.Range(1f, 1.5f),
+      transform.position.y,
+      transform.position.z + Random.Range(3f, 4f));
+    }
+    else
+    {
+      spawnPosition = new Vector3(
+          transform.position.x + Random.Range(-2f, -1f),
+          transform.position.y,
+          transform.position.z + Random.Range(3f, 4f));
+    }
+
+    GameObject balloon = Instantiate(balloonPrefab, spawnPosition, Quaternion.identity) as GameObject;
+
+    // make this a child of the spawner so balloons will get destroyed when level is swapped out
+    balloon.transform.parent = gameObject.transform;
+
+    BalloonClick click = balloon.GetComponent<BalloonClick>();
+    click.spawner = this;
+
+    yield return null;
   }
 
   public void BalloonPopped()
   {
-        //spawned = true;
-        balloonpop += 1;
-        Debug.Log(balloonpop.ToString());
-        if (balloonpop == 2)
-        {
-            flow.PoppedBalloon();
-            swapped = true;
-        }
-        SpawnBalloon();
+    balloonpop += 1;
+    Debug.Log(balloonpop.ToString());
+    if (balloonpop == 2)
+    {
+      flow.PoppedBalloon();
+      swapped = true;
+    }
+    SpawnBalloon();
   }
 
-    /*void Update()
-    {
-        if (spawned)
-        {
-            balloonpop += 1;
-            Debug.Log(balloonpop.ToString());
-        }
-
-
-        if (balloonpop == 2)
-        {
-            CardAppear();
-        }
-    }*/
-    void CardAppear()
-    {
-        flow.PoppedBalloon();
-    }
+  void CardAppear()
+  {
+    flow.PoppedBalloon();
+  }
 }
