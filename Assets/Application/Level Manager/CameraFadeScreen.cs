7bde622809955d4777ebef6a2fbaa8079b73dd30
Steve Gehrman <sgehrman@gmail.com>
2015-10-20 22:21:44 -0700

    dfg

--- Assets/Application/Level Manager/CameraFadeScreen.cs
+++ Assets/Application/Level Manager/CameraFadeScreen.cs
@@ -9,10 +9,6 @@ public class CameraFadeScreen : MonoBehaviour
 
   const float fadeTime = .5f;
 
-  void Update()
-  {
-  }
-
   void Start()
   {
     material = GetComponent<MeshRenderer>().material;
