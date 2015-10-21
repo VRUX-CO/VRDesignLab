e045c878165278980563c0f104a2670bc07ec2df
Steve Gehrman <sgehrman@gmail.com>
2015-10-21 02:26:54 -0700

    working

--- Assets/Lab1_Flow.cs
+++ Assets/Lab1_Flow.cs
@@ -1,26 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Lab1_Flow : MonoBehaviour {
-    public GameObject Card1;
-    public GameObject Card2;
+public class Lab1_Flow : MonoBehaviour
+{
+  public GameObject Card1;
+  public GameObject Card2;
 
-    private int balloonpop;
-    
-	// Use this for initialization
-	void Start () {
-        
-        /*balloons = GameObject.FindGameObjectsWithTag("Ballons");
-        if (balloons.length > 20)
-        {
-            // Do Something
-        }*/
-    }
+  private int balloonpop;
 
-    // Update is called once per frame
-    public void PoppedBalloon()
+  // Use this for initialization
+  void Start()
+  {
+    /*balloons = GameObject.FindGameObjectsWithTag("Ballons");
+    if (balloons.length > 20)
     {
-        Destroy(Card1);
-        Instantiate(Card2, Card2.transform.position, Card2.transform.rotation);
-	}
+        // Do Something
+    }*/
+  }
+
+  // Update is called once per frame
+  public void PoppedBalloon()
+  {
+    Destroy(Card1);
+    GameObject newCard = Instantiate(Card2, Card2.transform.position, Card2.transform.rotation) as GameObject;
+
+    // parent this to gameObject so it gets cleaned up when level is unloaded
+    newCard.transform.parent = gameObject.transform;
+  }
 }
