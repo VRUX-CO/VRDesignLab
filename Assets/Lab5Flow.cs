83c4b3af64d0705e760b11c78c2a6833f517d5f2
L3Mantis <luis@mantisvr.com>
2015-10-21 16:49:30 -0700

    Lab 5

---
+++ Assets/Lab5Flow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lab5Flow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //OVRManager.capiHmd.ConfigureTracking(0, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
