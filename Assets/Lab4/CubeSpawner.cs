0aad00dab801b339fd123400acfd12720408d3ab
Steve Gehrman <sgehrman@gmail.com>
2015-10-22 01:29:44 -0700

    sdf

--- Assets/Lab4/CubeSpawner.cs
+++ Assets/Lab4/CubeSpawner.cs
@@ -4,37 +4,60 @@ using System.Collections;
 public class CubeSpawner : MonoBehaviour
 {
   public GameObject cube;
+  public GameObject floor;
+  public Material floorMat;
+  public Material floorMatLined;
+  bool linedMode = false;
+  MeshRenderer renderer;
+
   bool running = true;
   float xOffset = 3;
+  const float speed = 1f;
 
   void Start()
   {
+    renderer = floor.GetComponent<MeshRenderer>();
+
     StartCoroutine(SpawnCube(0));
+  }
+
+  void Update()
+  {
+    if (Utilities.UserClicked())
+    {
+      if (linedMode)
+      {
+        renderer.material = floorMat;
+      }
+      else
+      {
+        renderer.material = floorMatLined;
+      }
+
+      linedMode = !linedMode;
+    }
   }
 
   IEnumerator SpawnCube(int count)
   {
-    for (int x = 0; x < 1; x++)
+    float startZ = 70;
+    float xRandom = Random.RandomRange(3f, 22f);
+    float zRandom = Random.RandomRange(-10f, 10f);
+
+    Vector3 position = new Vector3(xOffset + xRandom, 0f, startZ + zRandom);
+    if ((count % 2) == 0)
     {
-      float startZ = 70;
-      float xRandom = Random.RandomRange(3f, 22f);
-      float zRandom = Random.RandomRange(-10f, 10f);
-
-      Vector3 position = new Vector3(xOffset + xRandom, 0f, startZ + zRandom);
-      if ((count % 2) == 0)
-      {
-        position = new Vector3(-xOffset - xRandom, 0f, startZ + zRandom);
-      }
-
-      Instantiate(cube, position, Quaternion.identity);
+      position = new Vector3(-xOffset - xRandom, 0f, startZ + zRandom);
     }
+
+    GameObject gameObject = Instantiate(cube, position, Quaternion.identity) as GameObject;
+    gameObject.GetComponent<CubeSpawnerCube>().StartMoving(speed);
 
     if (running)
     {
-      yield return new WaitForSeconds(.2f);
+      yield return new WaitForSeconds(.2f / speed);
 
       StartCoroutine(SpawnCube(count + 1));
     }
-
   }
 }
--- Assets/Lab4/CubeSpawnerCube.cs
+++ Assets/Lab4/CubeSpawnerCube.cs
@@ -3,31 +3,23 @@ using System.Collections;
 
 public class CubeSpawnerCube : MonoBehaviour
 {
-  // Use this for initialization
-  void Start()
+  const float startScale = 3f;
+  const float yScale = 40f;
+  const float moveTime = 12f;
+
+  public void StartMoving(float inSpeed = 1f)
   {
-    float startScale = 3f;
-    float yScale = 40f;
-    float moveTime = 12f;
-
-    Vector3 startPosition = transform.position;
-
     transform.localScale = new Vector3(startScale, 0, startScale);
 
-    iTween.ScaleTo(gameObject, new Vector3(startScale, yScale, startScale), Random.RandomRange(9f, 13f));
+    iTween.ScaleTo(gameObject, new Vector3(startScale, yScale, startScale), Random.RandomRange(9f, 13f) / inSpeed);
 
+    Vector3 startPosition = transform.position;
     startPosition.z = -4;
-    iTween.MoveTo(gameObject, iTween.Hash("position", startPosition, "time", moveTime, "easeType", iTween.EaseType.linear, "oncomplete", "OnCompleteCallback"));
-
-
-
-
-
+    iTween.MoveTo(gameObject, iTween.Hash("position", startPosition, "time", moveTime / inSpeed, "easeType", iTween.EaseType.linear, "oncomplete", "OnCompleteCallback"));
   }
 
-  public void OnCompleteCallback()
+  void OnCompleteCallback()
   {
     Destroy(gameObject);
   }
-
 }
