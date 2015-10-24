77f7d6e3b4c0ff088ee6d32442816ccc220dd35e
Steve Gehrman <sgehrman@gmail.com>
2015-10-24 14:28:23 -0700

    balloon pop sound fixed

--- Assets/Balloon Spawner/BalloonClick.cs
+++ Assets/Balloon Spawner/BalloonClick.cs
@@ -5,38 +5,27 @@ public class BalloonClick : MonoBehaviour
 {
   public BalloonSpawner spawner;
   public SplitMeshIntoTriangles balloonsplit;
-  
 
-  
-
-    void Start()
-    {
-        iTween.MoveBy(gameObject, iTween.Hash("y", .1f, "time", 1f, "easeType", iTween.EaseType.easeInOutSine, "loopType", iTween.LoopType.pingPong));
-    }
+  void Start()
+  {
+    iTween.MoveBy(gameObject, iTween.Hash("y", .1f, "time", 1f, "easeType", iTween.EaseType.easeInOutSine, "loopType", iTween.LoopType.pingPong));
+  }
 
   public void OnClick()
   {
-    // show particles
-    // gameObject.GetComponentInChildren<ParticleSystem>().Play();
-
     //Split the Balloon 
     balloonsplit.SplitBalloon();
+
     // play pop sound
-    if (gameObject.GetComponent<AudioSource>() != null) gameObject.GetComponent<AudioSource>().Play();
+    if (gameObject.GetComponent<AudioSource>() != null)
+      gameObject.GetComponent<AudioSource>().Play();
 
     // tell the spawner we've been popped
     spawner.BalloonPopped();
 
-        // destroy everything after delay to give time for particle system to finish
-        /*int destroyDelay = 1;
-        foreach (Transform child in this.transform)
-        {
-          Destroy(child.gameObject, destroyDelay);
-        }
-        Destroy(this, destroyDelay);*/
-        
-        Destroy(gameObject);
-    
-    }
+    // give time for the sound to play
+    Destroy(gameObject, 1);
 
- }
+  }
+
+}
