78aa93f9a2d5733a0c31251c2c5e5b85678c5433
Steve Gehrman <sgehrman@gmail.com>
2015-10-26 20:48:09 -0700

    sdf

--- Assets/Lab2/DistanceRing.cs
+++ Assets/Lab2/DistanceRing.cs
@@ -4,15 +4,25 @@ using System.Collections;
 public class DistanceRing : MonoBehaviour
 {
   public Material torusMaterial;
+  public GameObject line;
+  public float meters;
+  public float lineLength;
 
   // Use this for initialization
   void Start()
   {
-    MeshFilter filter = MeshUtilities.AddTorusMeshFilter(gameObject);
+    float radius = meters / 2f;
+    float radius2 = radius / 20f;
+    int nbRadSeg = 44;
+    int nbSides = 18;
 
+    MeshFilter filter = MeshUtilities.AddTorusMeshFilter(gameObject, radius, radius2, nbRadSeg, nbSides);
     MeshRenderer renderer = gameObject.AddComponent<MeshRenderer>();
 
     renderer.material = torusMaterial;
+
+    line.transform.localScale = new Vector3(.03f, lineLength, 1);
+    line.transform.localPosition = new Vector3(0, lineLength / 2f, radius);
   }
 
   // Update is called once per frame
