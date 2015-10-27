f8e674546ef9526d7b5bb56a71e1e819de096c75
Steve Gehrman <sgehrman@gmail.com>
2015-10-26 19:52:53 -0700

    adsf

---
+++ Assets/Application/Scripts/FaceCameraScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceCameraScript : MonoBehaviour
+{
+
+  // Use this for initialization
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    Utilities.RotateToFaceCamera(transform, Camera.main);
+  }
+}
