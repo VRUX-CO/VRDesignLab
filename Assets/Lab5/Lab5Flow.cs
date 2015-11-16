858d8643766e1f85832386ffaca5782ed9a41d8a
Steve Gehrman <sgehrman@gmail.com>
2015-11-16 15:23:29 -0800

    restored to dk2

--- Assets/Lab5/Lab5Flow.cs
+++ Assets/Lab5/Lab5Flow.cs
@@ -8,7 +8,6 @@ public class Lab5Flow : MonoBehaviour
   // Use this for initialization
   void Start()
   {
-    //OVRManager.capiHmd.ConfigureTracking(0, 0);
   }
 
   // Update is called once per frame
