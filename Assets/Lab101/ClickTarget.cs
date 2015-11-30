88103de755eb4439565d7e84ba78a989c54ef3ed
Steve Gehrman <sgehrman@gmail.com>
2015-11-29 18:53:18 -0800

    sdf

---
+++ Assets/Lab101/ClickTarget.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickTarget : MonoBehaviour
+{
+  public GameObject targetPrefab;
+
+  // private
+  GameObject clickDelegate;
+  string clickCallback;
+  GameObject targetModel;
+  string targetID;
+
+  void Update()
+  {
+  }
+
+  // Use this for initialization
+  void Start()
+  {
+    targetModel = Instantiate(targetPrefab);
+
+    targetModel.transform.parent = transform;  // parent this quad
+    targetModel.transform.localPosition = Vector3.zero;
+
+    targetModel.tag = Crosshair3D.kCrosshairTargetable;
+
+    EventDelegate ed = targetModel.AddComponent<EventDelegate>();
+    ed.eventDelegate = gameObject;
+
+    targetModel.transform.localScale = new Vector3(22, 22, 22);
+
+  }
+
+  public void SetClickDelegate(GameObject del, string callb, string inTargetID)
+  {
+    clickDelegate = del;
+    clickCallback = callb;
+
+    targetID = inTargetID;
+  }
+
+  public void OnClick()
+  {
+    if (clickDelegate != null)
+    {
+      clickDelegate.SendMessage(clickCallback, targetID, SendMessageOptions.DontRequireReceiver);
+    }
+  }
+
+  public void OnHoverStart()
+  {
+  }
+
+  public void OnHoverEnd()
+  {
+  }
+
+}
--- Assets/Lab101/Lab101Controller.cs
+++ Assets/Lab101/Lab101Controller.cs
@@ -3,17 +3,32 @@ using System.Collections;
 
 public class Lab101Controller : MonoBehaviour
 {
+  public GameObject waypointTarget;
 
   // Use this for initialization
   void Start()
   {
     AppCentral.APP.ShowEnvironment(EnvironmentEnum.kForest);
+
+    CreateClickTarget("c", new Vector3(0, 2, 1));
+    CreateClickTarget("cd", new Vector3(-3, 2, 1));
+    CreateClickTarget("ce", new Vector3(3, 2, 1));
+
   }
 
-  // Update is called once per frame
-  void Update()
+  void TargetClicked(string targetID)
   {
+    Debug.Log(targetID);
+  }
 
+  void CreateClickTarget(string targetID, Vector3 position)
+  {
+    GameObject target = Instantiate(waypointTarget, position, Quaternion.identity) as GameObject;
+    ClickTarget clickTarget = target.GetComponent<ClickTarget>();
+
+    clickTarget.SetClickDelegate(gameObject, "TargetClicked", targetID);
+
+    target.transform.parent = transform;
   }
 }
 
