16065c5a3d83fab68e5a39a9ea3b9a512bdf6963
cybutek <admin@cybutek.net>
2015-12-11 14:36:54 +0000

    Created FixWorldRotation component and implemented it on the cart prefabs.

---
+++ Assets/Application/Scripts/FixWorldRotation.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class FixWorldRotation : MonoBehaviour
+{
+    [SerializeField]
+    private bool x = false;
+
+    [SerializeField]
+    private bool y = false;
+
+    [SerializeField]
+    private bool z = false;
+
+    private Vector3 originalRotation;
+
+    protected virtual void LateUpdate()
+    {
+        if (x || y || z)
+        {
+            Vector3 currentRotation = transform.eulerAngles;
+
+            if (x)
+            {
+                currentRotation.x = originalRotation.x;
+            }
+
+            if (y)
+            {
+                currentRotation.y = originalRotation.y;
+            }
+
+            if (z)
+            {
+                currentRotation.z = originalRotation.z;
+            }
+
+            transform.eulerAngles = currentRotation;
+        }
+    }
+
+    protected virtual void Start()
+    {
+        Reset();
+    }
+
+    private void Reset()
+    {
+        originalRotation = transform.eulerAngles;
+    }
+}
