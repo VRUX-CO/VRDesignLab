ab3efef10cfdfb30694bf7643c34c040a46989e9
BonozoDev <bonozogames@gmail.com>
2016-01-14 18:27:54 +0000

    Have only one enabled directional light source active. (Floor glitch fix 1/2)

---
+++ Assets/Application/Scripts/LightStack.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+public class LightStack : MonoBehaviour
+{
+    private static List<Light> lights = new List<Light>();
+
+    private new Light light;
+
+    protected virtual void Awake()
+    {
+        light = GetComponent(typeof(Light)) as Light;
+    }
+
+    protected virtual void OnDestroy()
+    {
+        lights.Remove(light);
+        RefreshStack();
+    }
+
+    protected virtual void Start()
+    {
+        lights.Add(light);
+        RefreshStack();
+    }
+
+    private static void RefreshStack()
+    {
+        for (int i = 0; i < lights.Count; ++i)
+        {
+            lights[i].enabled = (i == lights.Count - 1);
+        }
+    }
+}
